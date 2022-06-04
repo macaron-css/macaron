9a959689c17d8a53539d31672a4cfc1c143df36f
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2022-06-04 21:20:16 +0530

    variant inheritance and debug names styled api

--- packages/solid/src/runtime.ts
+++ packages/solid/src/runtime.ts
@@ -1,31 +1,35 @@
 import { createComponent, mergeProps, splitProps } from 'solid-js';
 import { Dynamic } from 'solid-js/web';
 
-export function $$styled(component: any, styles = (_: any) => ({})) {
-  return function StyledComponent(props: any) {
-    // TODO: split props into variants and rest
-    // waiting for https://github.com/seek-oss/vanilla-extract/pull/712
-    // to get resolved
-
-    // const [classes, others] = splitProps(props, ['variants']);
+export function $$styled(
+  component: any,
+  styles: ((_: any) => string) & { variants: string[] }
+) {
+  function StyledComponent(props: any) {
+    const [classes, others] = splitProps(props, [
+      ...styles.variants,
+      component.variants,
+    ]);
 
     return createComponent(
       Dynamic as any,
-      mergeProps(
-        {
-          component,
-          get ['class']() {
-            let className = styles(props);
+      mergeProps(others, {
+        component,
+        get ['class']() {
+          let className = styles(classes);
 
-            if (props.class) {
-              className += ` ${props.class}`;
-            }
+          if (props.class) {
+            className += ` ${props.class}`;
+          }
 
-            return className;
-          },
+          return className;
         },
-        props
-      )
+      })
     );
-  };
+  }
+
+  StyledComponent.toString = () => `Styled(${component})`;
+  StyledComponent.variants = styles.variants;
+
+  return StyledComponent;
 }
