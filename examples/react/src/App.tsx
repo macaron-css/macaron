40e0f790e5ba4a30b39a81869b0f43ec6a6c0801
Kavin Valli <mail@kavin.me>
2022-09-10 15:22:35 +0530

    chore(examples): move react example to one file

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,21 +1,85 @@
 import { styled, StyleVariants } from "@macaron-css/react";
 import { style } from "@macaron-css/core";
-import { Button, color, fontFamily } from "./Button";
+
+const color = "red";
+const fontFamily = "sans-serif";
+
+const Button = styled("button", {
+  base: {
+    borderRadius: 4,
+    border: 0,
+    margin: 12,
+    cursor: "pointer",
+    color: "white",
+    textTransform: "uppercase",
+    fontSize: 12,
+  },
+
+  variants: {
+    color: {
+      neutral: { background: "whitesmoke", color: "#333" },
+      brand: { background: "blueviolet" },
+      accent: { background: "slateblue" },
+    },
+    size: {
+      small: { padding: 12 },
+      medium: { padding: 16 },
+      large: { padding: 24 },
+    },
+    rounded: {
+      true: { borderRadius: 999 },
+    },
+  },
+
+  // Applied when multiple variants are set at once
+  compoundVariants: [
+    {
+      variants: {
+        color: "neutral",
+        size: "large",
+      },
+      style: {
+        background: "ghostwhite",
+      },
+    },
+  ],
+
+  defaultVariants: {
+    color: "accent",
+    size: "medium",
+  },
+});
 
 function App() {
   return (
     <div className={style({ color, fontFamily })}>
       <p>Hello World!</p>
-      <Button color="neutral" size="small">
+      <Button
+        color="neutral"
+        size="small"
+        onClick={() => console.log("Clicked 1")}
+      >
         Click Me
       </Button>
-      <Button color="brand" size="medium">
+      <Button
+        color="brand"
+        size="medium"
+        onClick={() => console.log("Clicked 2")}
+      >
         Click Me
       </Button>
-      <Button color="accent" size="large">
+      <Button
+        color="accent"
+        size="large"
+        onClick={() => console.log("Clicked 3")}
+      >
         Click Me
       </Button>
-      <Button rounded="true" size="small">
+      <Button
+        rounded="true"
+        size="small"
+        onClick={() => console.log("Clicked 4")}
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
