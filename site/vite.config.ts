301cbd4fd67535dd3e9157448757b1a19ebf5b52
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2022-11-03 20:36:04 +0530

    fix

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,6 +1,7 @@
 import solid from 'solid-start/vite';
 import { defineConfig } from 'vite';
 import { macaronVitePlugin } from '@macaron-css/vite';
+import netlify from 'solid-start-netlify';
 
 export default defineConfig({
   plugins: [
@@ -15,6 +16,7 @@ export default defineConfig({
     macaronVitePlugin(),
     solid({
       extensions: ['.mdx', '.md'],
+      adapter: netlify(),
     }),
   ],
 });
